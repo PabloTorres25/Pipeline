a75f1396d825e8e6a0df0d247d452e27c83e2a52
PabloTorres25 <a01753412@tec.mx>
2024-05-23 22:32:18 +0000

    cliente Listo

--- src/modelsNoSQL/ClienteModel.ts
+++ src/modelsNoSQL/ClienteModel.ts
@@ -2,28 +2,25 @@ import dynamodb from "../services/dynamodbService";
 import joi from "joi";
 import { PREFIX_NAME } from "../config";
 
-const AgenteModel = dynamodb.define("agente", {
-  hashKey: "idAgente",
+const ClienteModel = dynamodb.define("cliente", {
+  hashKey: "idCliente",
   timestamps: true,
   schema: {
-    idAgente: dynamodb.types.uuid(),
+    idCliente: dynamodb.types.uuid(),
     nombre: joi.string().required(),
-    apellidoP: joi.string().required(),
-    apellidoM: joi.string().required(),
-    fechaNacimiento: joi.date().required(),
     genero: joi.string().required(),
     correo: joi.string().email().required(),
     password: joi.string().required()
   },
-  tableName: Agente${PREFIX_NAME},
+  tableName: `Cliente${PREFIX_NAME}`,
 });
 
 dynamodb.createTables(function (err) {
   if (err) {
     console.error("Error creating tables: ", err);
   } else {
-    console.log("Table Agente has been created");
+    console.log("Table Cliente has been created");
   }
 });
 
-export default AgenteModel;
+export default ClienteModel;
