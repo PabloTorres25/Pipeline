432cc19a013cc596d2db049324374941ed232e5e
PabloTorres25 <a01753412@tec.mx>
2024-05-23 22:49:37 +0000

    Index Base Listo

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
-function saludar(nombre: string): string {
-    return `Hola, ${nombre}!`;
-  }
-  
-  const nombre: string = "Mundo";
-  console.log(saludar(nombre));
-  
+import Server from "./src/providers/Server";
+import {PORT,NODE_ENV} from './src/config';
+import express from 'express';
+import ProductoController from "./src/controllers/ProductoController";
+
+const server = new Server({
+    port:PORT,
+    env:NODE_ENV,
+    middlewares:[
+        express.json(),
+        express.urlencoded({extended:true})
+    ],
+    controllers:[
+      ProductoController.instance
+    ]
+    
+});
+
+server.init();
